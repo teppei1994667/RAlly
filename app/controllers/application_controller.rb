be7d11c9a7fe1cd31615a302dad9a2aa3ea9c7c4
teppei1994667 <teppei.engineer.667@gmail.com>
2022-06-21 23:15:58 +0900

    新規登録画面にshop_nameのformを追加しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  #もしdeviseのコントローラーの事なら全てのアクションの前にメソッドを読み込む
+  before_action :configure_signup_parameters, if: :devise_controller? #新規登録の場合
+
+  private
+  #新規登録機能について
+  def configure_signup_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:shop_name])
+  end
+
 end
