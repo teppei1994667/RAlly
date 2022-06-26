bf8f6ea5593a87bc81c34cc99059e0594033e273
teppei1994667 <teppei.engineer.667@gmail.com>
2022-06-26 17:36:51 +0900

    application_controller.rbのconfigure_signup_parametersのコメントを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_signup_parameters, if: :devise_controller? #新規登録の場合
 
   private
-  #新規登録機能について
+  #新規登録機能でshop_nameのパラメーターを受け取れるように設定
   def configure_signup_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:shop_name])
   end
