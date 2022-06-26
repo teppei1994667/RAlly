dec23175ae15b1ffa114cc62b5ce3b03ceb6b6d8
teppei1994667 <teppei.engineer.667@gmail.com>
2022-06-27 00:28:39 +0900

    Home画面のアクセス制限のテストを追加しました

--- spec/system/homes_spec.rb
+++ spec/system/homes_spec.rb
@@ -1,12 +1,29 @@
 require 'rails_helper'
 
-describe 'ログイン、新規登録リンク表示', type: :system do
-  context 'ホーム画面にアクセスした時' do
+describe 'Home画面表示', type: :system do
+  before do
+    FactoryBot.create(:shop, shop_name: 'ショップA', email: 'a@example.com')
+  end
+
+  context 'ログインしていない状態でHome画面にアクセスした時' do
     before do
       visit root_path
     end
     it 'ログインページへのリンクが表示される' do
       expect(page).to have_link 'ログイン', href: "/shops/sign_in"
+    end
+  end
+
+  context 'ログインしている状態でHome画面にアクセスした時' do
+    before do
+      visit new_shop_session_path
+      fill_in 'shop_email', with: 'a@example.com'
+      fill_in 'shop_password', with: 'password'
+      click_button 'ログイン'
+      visit root_path
+    end
+    it 'ログインしているshopのshowページにリダイレクトする' do
+      expect(page).to have_content 'ショップA'
     end
   end
 end
