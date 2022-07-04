d2532c7383884571e291493c55a1df765e238a7a
teppei1994667 <teppei.engineer.667@gmail.com>
2022-07-04 23:25:42 +0900

    OrderStoreのマイグレーションとorder_storesコントローラーの作成を実行しました

--- db/migrate/20220704134152_create_order_stores.rb
+++ db/migrate/20220704134152_create_order_stores.rb
@@ -2,7 +2,7 @@ class CreateOrderStores < ActiveRecord::Migration[6.1]
   def change
     create_table :order_stores do |t|
       t.references :shop, null: false, foreign_key: true
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
