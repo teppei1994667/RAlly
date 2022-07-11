fe70a5060a8c6bc86dae41a7a57f350b03c532b4
teppei1994667 <teppei.engineer.667@gmail.com>
2022-07-12 00:36:32 +0900

    order_storeのindexページに新規作成ページ(new)のリンクを追加しました

--- app/controllers/order_stores_controller.rb
+++ app/controllers/order_stores_controller.rb
@@ -1,11 +1,14 @@
 class OrderStoresController < ApplicationController
   def index
+    @shop = current_shop
   end
 
   def show
   end
 
   def new
+    @shop = current_shop
+    @order_store = OrderStore.new
   end
 
   def edit
