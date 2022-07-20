a6880b9d9fb54460bcad3533be7c0ac82361ca77
teppei1994667 <teppei.engineer.667@gmail.com>
2022-07-21 00:51:48 +0900

    shopモデルのバリデーションのテストを追加しました

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,8 @@
 class Shop < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
   has_many :order_stores
+  validates :shop_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: {minimum: 6, maximum: 30}
 end
